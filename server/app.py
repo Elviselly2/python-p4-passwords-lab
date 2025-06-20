bf8f44a1e397be875c52b4a29c9ac969e732a2b7
nosmall <elvisotieno24@gmail.com>
2025-06-20 18:36:44 +0300

    adds content

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import request, session
 from flask_restful import Resource
 
 from config import app, db, api
-from models import User
+from models import User,bcrypt
 
 class ClearSession(Resource):
 
@@ -28,16 +28,42 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
+    def get(self):
+        user_id=session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
+
     pass
 
 class Login(Resource):
+    def post(self):
+       
+
+        json=request.get_json()
+        username=json.get('username')
+        password=json.get('password')
+        user=User.query.filter(username==username).first()
+        if user and user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {'error': 'Invalid credentials'}, 401
     pass
 
 class Logout(Resource):
+    def delete(self):
+        session.pop('user_id',None)
+        return {},204
     pass
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5666, debug=True)
